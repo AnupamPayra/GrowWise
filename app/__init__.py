23075aa6c0847d541ca52a310852ed8ab0545ad4
Anupam <anupampayra5@gmail.com>
2025-09-10 17:15:47 +0530

    First route

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def cretae_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'my_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app) #it responsible for creating the data base
+    
+    from app.routes.auth import auth_bp
+    # from app.routes.task import task_bp
+    app.register_blueprint(auth_bp)
+    # app.register_blueprint(task_bp)
+
+
+    return app
---
+++ run.py
@@ -0,0 +1,12 @@
+from app import cretae_app, db
+from app.models import Task, Users_Data
+
+
+app = cretae_app()
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
